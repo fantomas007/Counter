869dd7aa6af650fcf2cc7e9b97a82157f690f7fd
DG <grizadenis@icloud.com>
2023-08-22 23:16:11 +0300

    Создание проекта counter

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count = 0
+    func increaseCount() {
+        count += 1
+        labelCount.text = "Значение счетчика: \(count)"
+        
+    }
+    
+    @IBOutlet weak var buttonCount: UIButton!
+    @IBOutlet weak var labelCount: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        labelCount.text = "Значение счетчика: 0"
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonUp(_ sender: UIButton) {
+        increaseCount()
+    }
 }
+
 
